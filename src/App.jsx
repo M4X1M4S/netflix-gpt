54f7139b67f3bc028856d9e05abefe2b5e09f3b0
ABHILASH <99235816+M4X1M4S@users.noreply.github.com>
2025-03-25 12:41:44 +0530

    SignIn/SignUp && Header/Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Body from './components/Body'
 
 const App = () => {
   return (
-    <div className='bg-red-200'>App</div>
+    <Body/>
   )
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Header from "./Header";
+
+const Login = () => {
+  const toggleSignUp = () => {
+    setSignUp(!isSignUp);
+  };
+  const [isSignUp, setSignUp] = useState(false);
+  return (
+    <div className="relative w-full h-screen">
+      <Header />
+
+      <img
+        className=" w-full h-screen object-cover"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/f6e7f6df-6973-46ef-b98f-12560d2b3c69/web/IN-en-20250317-TRIFECTA-perspective_26f87873-6014-460d-a6fb-1d96d85ffe5f_medium.jpg"
+        alt="Login-bg"
+      />
+      <form className="w-[25%] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  bg-black/70 p-6 rounded-xl text-white">
+        <h1 className="text-3xl text-center font-medium pt-5">
+          {isSignUp ? "Sign Up" : "Sign In"}
+        </h1>
+
+        {isSignUp && (
+          <input
+            className="border-1 border-gray-600 block mx-auto w-[85%] mt-5 h-11 bg-gray-600 pl-3"
+            type="Name"
+            name="Name"
+            placeholder="Enter your name"
+            required
+          />
+        )}
+        <input
+          className="border-1 border-gray-600 block mx-auto w-[85%] mt-5 h-11 bg-gray-600 pl-3"
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          required
+        />
+        <input
+          className="border-1 border-gray-600 rounded block mx-auto w-[85%] mt-5 h-11 bg-gray-600 pl-3"
+          type="password"
+          name="password"
+          placeholder="Enter password"
+          required
+        />
+        <button className=" border-2 border-red-600 rounded-sm mx-auto block w-[85%] mt-5 mb-5 h-11 bg-red-600">
+          {isSignUp ? "Sign Up" : "Sign In"}
+        </button>
+        <p
+          className="ml-[7.5%] text-gray-400 cursor-pointer pb-5"
+          onClick={toggleSignUp}
+        >
+          {isSignUp
+            ? "Already a user? Sign in now "
+            : " New to Netflix? Sign up now"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;

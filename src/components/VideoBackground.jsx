edc64d488bcdebc609b4d3c66d5ec849edd1499a
ABHILASH <99235816+M4X1M4S@users.noreply.github.com>
2025-04-01 23:56:21 +0530

    Fix: Videobackgound is now full screen

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -7,10 +7,19 @@ const VideoBackground = ({ videoId }) => {
   console.log(trailer);
 
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
-      <iframe
-        className="absolute top-0 left-0 w-screen h-screen object-cover"
+    <div className=" text-9xl h-screen w-screen bg-gradient-to-r from-black to-white">
+      {/* <iframe
+        className="absolute inset-0 w-full h-full"
         src={`https://www.youtube.com/embed/${trailer?.key}?autoplay=1&mute=1&loop=1&playlist=${trailer?.key}&controls=0&showinfo=0&modestbranding=1&rel=0`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe> */}
+      <iframe
+        className="absolute top-1/2 left-1/2 w-[220vw] h-[135vh] -translate-x-1/2 -translate-y-1/2  "
+        src={`https://www.youtube.com/embed/${trailer.key}?autoplay=1&mute=1&loop=1&playlist=${trailer.key}&controls=0&showinfo=0&modestbranding=1&rel=0`}
         title="YouTube video player"
         frameBorder="0"
         allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="absolute z-40 px-24 pt-[10%] mt-[5%]">
-      <h1 className="text-7xl font-semibold">{title}</h1>
-      <p className="text-xl w-1/4">{overview}</p>
-      <button className="p-5 border-2 text-2xl font-medium mr-2 mt-2">
-        Play
+    <div className="absolute z-40 px-24 pt-[10%] mt-[5%] text-white ">
+      <h1 className="text-7xl font-semibold mb-4">{title}</h1>
+      <p className="text-xl w-1/4 mb-4">{overview}</p>
+      <button className=" px-5 p-3 border-2 text-2xl font-medium mr-2 mt-2">
+        ▸ Play
       </button>
-      <button className="p-5 border-2 text-2xl font-medium ml-2 mt-2">
-        More Info
+      <button className="p-3 border-2 text-2xl font-medium ml-2 mt-2">
+        ⓘ More Info
       </button>
     </div>
   );
